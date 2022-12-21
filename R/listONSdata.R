252287b8168b1ccbdf32297c706e56e22db63aff
Pete Wrixon <petewrixon@outlook.com>
2022-12-21 11:04:15 +0000

    Initial code to populate list of ONS Datasets

--- R/listONSdata.R
+++ R/listONSdata.R
@@ -1,1 +1,10 @@
 # listONSdata.R
+
+website <-"https://www.ons.gov.uk/timeseriestool" |> xml2::read_html()
+
+x <- website %>% html_element(css = "h2") %>% html_text2() %>% gsub(.,""," time series data") #%>% gsub(.,"",",")
+
+max <- website %>%
+  html_element(css = "ul.pagination > li:nth-last-child(2)") %>%
+  html_text() %>%
+  as.numeric()
