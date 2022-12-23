11ca7ebeab5ad685ca5082b2156675be8a39333b
Pete Wrixon <petewrixon@outlook.com>
2022-12-23 12:10:57 +0000

    move to native r pipe

--- R/listONSdata.R
+++ R/listONSdata.R
@@ -2,7 +2,15 @@
 
 website <-"https://www.ons.gov.uk/timeseriestool" |> xml2::read_html()
 
-x <- website %>% html_element(css = "h2") %>% html_text2() %>% gsub(.,""," time series data") #%>% gsub(.,"",",")
+ no_ts_data <- website |>
+   rvest::html_element(css = "h2") |>
+   rvest::html_text2() |>
+   gsub(pattern = " time series data",replacement = "") |>
+   gsub(pattern = ",", replacement ="") |>
+   as.numeric()
+
+ no_of_pages <- no_ts_data
+
 
 max <- website %>%
   html_element(css = "ul.pagination > li:nth-last-child(2)") %>%
